908ff612173402605c1af36cf91ecee540a5101e
SophieP21 <Sophie.Pierson@flemingcollege.ca>
2024-03-15 20:43:42 -0400

    Update old_people

--- old_people.py
+++ old_people.py
@@ -8,6 +8,7 @@ Usage:
 """
 import os
 from create_db import db_path, script_dir
+import sqlite3
 
 def main():
     old_people_list = get_old_people()
@@ -24,7 +25,13 @@ def get_old_people():
     """
     # TODO: Create function body
     # Hint: See example code in lab instructions entitled "Getting People Data from the Database"
-    return
+    conn = sqlite3.connect(db_path)
+    c = conn.cursor()
+    c.execute('''SELECT name, age FROM people WHERE age >= 50''')
+    old_people_list = c.fetchall()
+    conn.close()
+    return old_people_list
+    
 
 def print_name_and_age(name_and_age_list):
     """Prints name and age of all people in provided list
@@ -34,6 +41,8 @@ def print_name_and_age(name_and_age_list):
     """
     # TODO: Create function body
     # Hint: Use a for loop to iterate the list of tuples to print a sentence for each old person
+    for person in name_and_age_list:
+        print(f"{person[0]} is {person[1]} years old.")
     return
 
 def save_name_and_age_to_csv(name_and_age_list, csv_path):
@@ -45,6 +54,10 @@ def save_name_and_age_to_csv(name_and_age_list, csv_path):
     """
     # TODO: Create function body
     # Hint: In Lab 3, we converted a list of tuples into a pandas DataFrame and saved it to a CSV file
+    with open(csv_path, 'w') as file:
+        file.write('Name,Age\n')
+        for person in name_and_age_list:
+            file.write(f"{person[0]},{person[1]}\n")
     return
 
 if __name__ == '__main__':
